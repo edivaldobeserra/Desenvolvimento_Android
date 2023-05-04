e56397cf7886de60584ab4bf1df19352efe15e45
edyen <edivaldobeserra023@gmail.com>
2023-05-03 21:50:42 -0300

    Implementando o ArrayAdapter para uso com Spinner para listas nomes

--- ApplistaCurso/app/src/main/java/devandroid/edivaldo/applistacurso/controller/CursoController.java
+++ ApplistaCurso/app/src/main/java/devandroid/edivaldo/applistacurso/controller/CursoController.java
@@ -14,17 +14,28 @@ public class CursoController {
 
         listCursos= new ArrayList<Curso>();
 
-        listCursos.add(new Curso());
-        listCursos.add(new Curso());
-        listCursos.add(new Curso());
-        listCursos.add(new Curso());
-        listCursos.add(new Curso());
-        listCursos.add(new Curso());
-        listCursos.add(new Curso());
+        listCursos.add(new Curso("Java"));
+        listCursos.add(new Curso("Php"));
+        listCursos.add(new Curso("Python"));
+        listCursos.add(new Curso("Html"));
+        listCursos.add(new Curso("Css"));
+        listCursos.add(new Curso("Ruby"));
+        listCursos.add(new Curso("Kotlin"));
 
         return listCursos;
 
+    }
 
+    public ArrayList<String> dadosParaSpinner(){
+        ArrayList<String> dados = new ArrayList<>();
+
+        for (int i = 0; i < getListaDeCursos().size();i++){
+
+            Curso objeto = (Curso) getListaDeCursos().get(i);
+            dados.add(objeto.getNomeDoCursoDesejado());
+
+        }
+        return dados;
 
     }
 }
--- ApplistaCurso/app/src/main/java/devandroid/edivaldo/applistacurso/model/Curso.java
+++ ApplistaCurso/app/src/main/java/devandroid/edivaldo/applistacurso/model/Curso.java
@@ -4,6 +4,11 @@ public class Curso {
 
     private String nomeDoCursoDesejado;
 
+    public Curso(String nomeDoCursoDesejado)
+    {
+        this.nomeDoCursoDesejado = nomeDoCursoDesejado;
+    }
+
     public String getNomeDoCursoDesejado() {
         return nomeDoCursoDesejado;
     }
