818f96261c772a631b5fa6a517602a2c8b058532
edyen <edivaldobeserra023@gmail.com>
2023-05-06 09:10:35 -0300

    Implementando uma tela Splash e trabalhando com duas Telas.

---
+++ ApplistaCurso/app/src/main/java/devandroid/edivaldo/applistacurso/view/SplashAcitivity.java
@@ -0,0 +1,34 @@
+package devandroid.edivaldo.applistacurso.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import devandroid.edivaldo.applistacurso.R;
+
+public class SplashAcitivity extends AppCompatActivity {
+
+    public static final int TIME_OUT_SPLASH = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        comutarTelaSolash();
+    }
+
+    private void comutarTelaSolash() {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent telaPrincipal = new Intent(SplashAcitivity.this, MainActivity.class);
+                startActivity(telaPrincipal);
+                finish();
+            }
+        },TIME_OUT_SPLASH);
+    }
+}
